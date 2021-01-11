41f8e09e2de68ba3892f68c918dce44a2b017b76
imnirfn <s3ns3xy@yahoo.com>
2021-01-11 19:01:04 +0800

    latest push

--- server.py
+++ server.py
@@ -12,7 +12,6 @@ class server(object):
     def __init__(self, host, port, wsport):
         self.host = host
         self.port = port
-        self.wsport = wsport
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.host, self.port))
@@ -21,14 +20,16 @@ class server(object):
         self.socket.listen(7)
         while True:
             client, addr = self.socket.accept()
+            print('Connection from: ' str(addr))
             start_new_thread(self.listenToClient, (client, addr))
 
     def listenToClient(self, client, addr):
-        data = client.recv(1024)
-        print('data', data)
-        sendmsg = self.server()
-        bytes = sendmsg.encode()
-        self.sendOverWebsocket(bytes)
+        val = client.recv(1024).decode('utf-8')
+        if val != None:
+            sendmsg = self.handler(val)
+            bytes = sendmsg.encode()
+            # upgrade raw socket connection to ws
+            sendOverWebsocket(bytes)
 
     def sendOverWebsocket(self, bytes):
         async def sendMsg(websocket, path):
@@ -38,18 +39,34 @@ class server(object):
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
-    def server(self):
+    def handler(self, val):
         # TO-DO
         url = 'http://localhost:3000/notes'
-        response = requests.get(url)
-        print(response.text)
-        return response.text
-
+        if val.type == 'gn':
+            res = requests.get(url)
+            return res.text
+        elif val.type == 'pn':
+            requests.patch(url, data = {
+                title: val.title,
+                description: val.description,
+                isComplete: val.isComplete,
+                userId: val.userId
+            })
+            res = requests.get(url)
+            return res.text
+        elif data.type == 'an':
+            requests.post(url, data = {
+                title: val.title,
+                description: val.description,
+                isComplete: val.isComplete,
+                userId: val.userId
+            })
+            res = requests.get(url)
+            return res.text
 
 if __name__ == '__main__':
     try:
         serve = server('localhost', 13337, 13338)
-        print('Server started')
         serve.listen()
     except socket.error as e:
         print("Error here: ", str(e))
